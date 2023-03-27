6705598994a20fa1a596f368327632cea991e923
yuval59 <yuvalmaron222@gmail.com>
2023-03-27 23:12:41 +0300

    Checking convergence of unit vector distance as function of dimensions
    Seems to converge to 0.707...
    I guess I'm gonna go prove this now ¯\_(ツ)_/¯

--- src/n-dimensions/index.ts
+++ src/n-dimensions/index.ts
@@ -1,21 +1,14 @@
 import { VectorN } from './classes/vector'
 import { getMulberry32 } from './useful-math'
 
-const dimensionsToTest = 95000
+const dimensionsToTest = 9500000
 const magnitudeToTest = 1
 const randFunction = getMulberry32(128)
 const pairsToCheck = 50
 
-// for (let i = 0; i < pairsToCheck; i++) {
-//   const a = VectorN.random(dimensionsToTest, magnitudeToTest, randFunction)
-//   const b = VectorN.random(dimensionsToTest, magnitudeToTest, randFunction)
+for (let i = 0; i < pairsToCheck; i++) {
+  const a = VectorN.random(dimensionsToTest, magnitudeToTest, randFunction)
+  const b = VectorN.random(dimensionsToTest, magnitudeToTest, randFunction)
 
-//   console.log(VectorN.getDistance(a, b))
-// }
-
-const a = VectorN.random(2, magnitudeToTest, randFunction)
-const b = VectorN.random(3, magnitudeToTest, randFunction)
-console.log(VectorN.getDistance(a, b))
-
-console.log(a.Values)
-console.log(b.Values)
+  console.log(VectorN.getDistance(a, b))
+}
