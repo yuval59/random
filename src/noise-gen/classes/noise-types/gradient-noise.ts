2a042da1f2ed79a917ae24b220fed67287b86177
yuval59 <yuvalmaron222@gmail.com>
2023-03-28 18:25:33 +0300

    Minor style changes to gradient-noise

--- src/noise-gen/classes/noise-types/gradient-noise.ts
+++ src/noise-gen/classes/noise-types/gradient-noise.ts
@@ -9,8 +9,8 @@ export class GradientNoise implements NoiseMapInterface {
   #vectorMap: Vector2[][]
 
   constructor(seed: number) {
+    this.#randomnessFunction = getMulberry32(seed)
     this.#seed = seed
-    this.#randomnessFunction = getMulberry32(this.#seed)
 
     this.#vectorMap = Array.from({ length: this.Size }, () =>
       this.#makeInsideArr()
@@ -34,10 +34,6 @@ export class GradientNoise implements NoiseMapInterface {
   get randomNumber() {
     return this.#randomnessFunction()
   }
-
-  getVector(x: number, y: number): Vector2 {
-    return this.#vectorMap[x][y]
-  }
   //#endregion
 
   //#region Math stuff
@@ -51,10 +47,10 @@ export class GradientNoise implements NoiseMapInterface {
     x = x * (this.Size - 1) - xOffset
     y = y * (this.Size - 1) - yOffset
 
-    const n00 = this.getVector(x, y).dotProduct(xOffset, yOffset)
-    const n01 = this.getVector(x, y + 1).dotProduct(xOffset, yOffset - 1)
-    const n10 = this.getVector(x + 1, y).dotProduct(xOffset - 1, yOffset)
-    const n11 = this.getVector(x + 1, y + 1).dotProduct(
+    const n00 = this.#vectorMap[x][y].dotProduct(xOffset, yOffset)
+    const n01 = this.#vectorMap[x][y + 1].dotProduct(xOffset, yOffset - 1)
+    const n10 = this.#vectorMap[x + 1][y].dotProduct(xOffset - 1, yOffset)
+    const n11 = this.#vectorMap[x + 1][y + 1].dotProduct(
       xOffset - 1,
       yOffset - 1
     )
