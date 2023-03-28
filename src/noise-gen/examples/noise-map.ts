6867f21da79f54f6e12b6473417f247b052416d0
yuval59 <yuvalmaron222@gmail.com>
2023-03-28 04:22:35 +0300

    Moved main example to examples/noise-map.ts + Added min/max values

--- src/noise-gen/examples/noise-map.ts
+++ src/noise-gen/examples/noise-map.ts
@@ -1,26 +1,38 @@
 import fs from 'fs'
+import { getFBM } from '../classes/fBm'
 import { GradientNoise, ValueNoise } from '../classes/noise'
+import { getMulberry32 } from '../useful-math'
 
-const seed = 127
+// Noise map constants
+const octaves = 8
+const seed = 256
+// Generation constants
 const width = 500
 const height = 500
-const stepSize = 0.00003
+const stepSize = 0.003
 
-const noiseMap = new GradientNoise(seed)
+const noiseMap = getFBM(GradientNoise, octaves, getMulberry32(seed))
 
 const result: number[][] = []
+let max = -999
+let min = 999
 
 for (let x = 0; x < width; x++) {
   const newColumn: number[] = []
   for (let y = 0; y < height; y++) {
-    newColumn.push(noiseMap.getValueAt(stepSize * x, stepSize * y))
+    const val = noiseMap.getValueAt(stepSize * x, stepSize * y)
+
+    if (val < min) min = val
+    if (val > max) max = val
+
+    newColumn.push(val)
   }
   result.push(newColumn)
 }
 
 fs.writeFileSync(
-  'processing-noise-vis/data/image.json',
-  JSON.stringify({ data: result })
+  'processing-noise-vis/data/noise-map.json',
+  JSON.stringify({ min, max, data: result })
 )
 console.log('Finished writing value JSON')
-console.log(`new random number: ${noiseMap.randomNumber}`)
+console.log(`Min was ${min} and max was ${max}`)
