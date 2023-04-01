135620643480e3496ec8ac1abfd35f4a77a519c2
yuval59 <yuvalmaron222@gmail.com>
2023-04-01 20:46:17 +0300

    Added xoshiro128ss PRNG

--- src/noise-gen/utils/useful-math.ts
+++ src/noise-gen/utils/useful-math.ts
@@ -55,6 +55,26 @@ export function sfc32(
   }
 }
 
+export function xoshiro128ss(
+  a: number,
+  b: number,
+  c: number,
+  d: number
+): RandomnessFunction {
+  return (): number => {
+    var t = b << 9,
+      r = a * 5
+    r = ((r << 7) | (r >>> 25)) * 9
+    c ^= a
+    d ^= b
+    b ^= c
+    a ^= d
+    c ^= t
+    d = (d << 11) | (d >>> 21)
+    return (r >>> 0) / 4294967296
+  }
+}
+
 export function getMulberry32(seed: number): RandomnessFunction {
   return (): number => {
     let t = (seed += 0x6d2b79f5)
