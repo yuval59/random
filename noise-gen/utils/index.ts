b9240cb5202307aea5d388b9a77a83fe15d0a1b3
Yuval Maron <yuvalmaron222@gmail.com>
2024-06-02 15:41:48 +0300

    Moved folders out of source

---
+++ noise-gen/utils/index.ts
@@ -0,0 +1,13 @@
+export {
+  cyrb128,
+  getFadeCurve,
+  getHashedL4PRNG,
+  getMulberry32,
+  jsf32,
+  lerp,
+  lerpCurve,
+  map_range,
+  sfc32,
+  xoshiro128ss,
+} from './useful-math'
+export { Vector2 } from './vector'
